5d9c30ebc1ab6ff1ac58c9f8d26b8fbf45abdaa9
Meirbek-dev <Meirbek.dev@gmail.com>
2025-04-09 19:09:23 +0500

    configure i18n

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -19,4 +19,8 @@ const nextConfig = {
   output: 'standalone',
 }
 
-module.exports = nextConfig
+import createNextIntlPlugin from 'next-intl/plugin'
+
+const withNextIntl = createNextIntlPlugin()
+
+export default withNextIntl(nextConfig)
