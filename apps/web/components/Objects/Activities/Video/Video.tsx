27d3efb5560f4a81841daaa6e1919016995137fd
Meirbek-dev <Meirbek.dev@gmail.com>
2025-06-05 21:26:59 +0500

    fix: fixed artplayer import

--- apps/web/components/Objects/Activities/Video/Video.tsx
+++ apps/web/components/Objects/Activities/Video/Video.tsx
@@ -5,7 +5,7 @@ import {
   getVideoSubtitlesDirectory,
 } from '@services/media/media'
 import { useOrg } from '@components/Contexts/OrgContext'
-import ArtPlayer from './ArtPlayer'
+import ArtPlayer from '@components/Objects/Activities/Video/ArtPlayer'
 import type ArtplayerType from 'artplayer'
 import { useLocale } from 'next-intl'
 
