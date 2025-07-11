d7870192431fb26536dd4919a251ecfffbdb7e8d
Meirbek-dev <meirbek.dev@gmail.com>
2025-07-11 15:42:20 +0500

    temporarily disabled email messages

--- apps/api/src/services/email/utils.py
+++ apps/api/src/services/email/utils.py
@@ -3,8 +3,10 @@ from pydantic import EmailStr
 
 from config.config import get_openu_config
 
-
 def send_email(to: EmailStr, subject: str, body: str):
+    print("We don't have email domain configured, so we're not sending emails, duh...")
+
+def future_send_email(to: EmailStr, subject: str, body: str):
     lh_config = get_openu_config()
     params = {
         "from": "OpenU <" + lh_config.mailing_config.system_email_address + ">",
