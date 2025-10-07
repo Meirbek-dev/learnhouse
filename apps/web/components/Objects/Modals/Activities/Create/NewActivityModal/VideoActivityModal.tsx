fa227c328337292df6213e83b3b38f488ab79ee5
Meirbek-dev <Meirbek.dev@gmail.com>
2025-10-07 11:48:52 +0500

    increased video file limit from 500mb to 1000mb

--- apps/web/components/Objects/Modals/Activities/Create/NewActivityModal/VideoActivityModal.tsx
+++ apps/web/components/Objects/Modals/Activities/Create/NewActivityModal/VideoActivityModal.tsx
@@ -881,8 +881,8 @@ const VideoModal = ({ submitFileActivity, submitExternalVideo, chapterId, course
     (event: ChangeEvent<HTMLInputElement>) => {
       const selectedFile = event.target.files?.[0];
       if (selectedFile) {
-        // Validate file size (max 500MB)
-        if (selectedFile.size > 500 * 1024 * 1024) {
+        // Validate file size (max 1000MB)
+        if (selectedFile.size > 1000 * 1024 * 1024) {
           toast.error(t('errorFileSizeLimit'));
           return;
         }
