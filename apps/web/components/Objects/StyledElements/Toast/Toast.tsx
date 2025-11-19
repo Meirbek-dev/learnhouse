682db265523f31b855093c9fe1c138261ef67830
Meirbek-dev <Meirbek.dev@gmail.com>
2025-11-19 11:47:17 +0500

    feat: add  zIndex to Toaster component for improved visibility

--- apps/web/components/Objects/StyledElements/Toast/Toast.tsx
+++ apps/web/components/Objects/StyledElements/Toast/Toast.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { Toaster } from 'react-hot-toast';
 
-const Toast = () => <Toaster />;
+const Toast = () => <Toaster containerStyle={{ zIndex: 99999 }} />;
 
 export default Toast;
