ee6aea7866fe63f2e154b8efa02b00e6b5239d25
Meirbek-dev <Meirbek.dev@gmail.com>
2025-12-12 10:04:23 +0500

    tweaked sonner

--- apps/web/components/ui/sonner.tsx
+++ apps/web/components/ui/sonner.tsx
@@ -5,18 +5,39 @@ import { Toaster as Sonner } from 'sonner';
 import type { ToasterProps } from 'sonner';
 import { useTheme } from 'next-themes';
 
-const Toaster = ({ ...props }: ToasterProps) => {
+const Toaster = ({ position = 'top-center', ...props }: ToasterProps) => {
   const { theme = 'system' } = useTheme();
 
   return (
     <Sonner
+      position={position}
       theme={theme as ToasterProps['theme']}
       className="toaster group"
       icons={{
-        success: <CircleCheckIcon className="size-4" />,
-        info: <InfoIcon className="size-4" />,
-        warning: <TriangleAlertIcon className="size-4" />,
-        error: <OctagonXIcon className="size-4" />,
+        success: (
+          <CircleCheckIcon
+            color="green"
+            className="size-4"
+          />
+        ),
+        info: (
+          <InfoIcon
+            color="blue"
+            className="size-4"
+          />
+        ),
+        warning: (
+          <TriangleAlertIcon
+            color="orange"
+            className="size-4"
+          />
+        ),
+        error: (
+          <OctagonXIcon
+            color="red"
+            className="size-4"
+          />
+        ),
         loading: <Loader2Icon className="size-4 animate-spin" />,
       }}
       style={
